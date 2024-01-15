65c8d5b67b097ea0e67c401a8e887c3a4b90359b
ItamarShmelo <itamargiron@gmail.com>
2024-01-15 18:01:30 +0200

    solver class + init function

--- solver.py
+++ solver.py
@@ -7,3 +7,25 @@ import os
 import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("SOLVER")
+
+class Solver:
+    def __init__(self, *, n:float, m:float):
+        self.n = n
+        self.m = m
+
+        self.beta = 2.0 + self.m * (self.n + 1.0)
+        self.b = 1.0 # sets the front at x=1 for t=-1
+
+        logger.info(f"n={self.n}")
+        logger.info(f"m={self.m}")
+
+        self.delta = None
+        
+        self.delta_temp = None
+        
+        self.event_lambda = lambda Z, V_arr, delta: zero_slope_event(Z, V_arr, m=self.m)
+        self.event_lambda.terminal = True
+
+        self.counter = 0
+        self.dirfigs = f"n_{self.n:.2f}_m_{self.m:.2f}"
+        os.makedirs(self.dirfigs, exist_ok=True)
